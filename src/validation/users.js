f685b743aee3fdb366241ad7d1aafa21b492435e
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-09-15 22:47:51 +0300

    update_joi-schema

--- src/validation/users.js
+++ src/validation/users.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const usersUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   gender: Joi.string().valid('woman', 'man'),
-  weight: Joi.number().positive().precision(1),
-  activeTime: Joi.number().positive().precision(2),
-  dailyNorm: Joi.number().positive().precision(0),
+  weight: Joi.number().positive().min(0).max(350).precision(1),
+  activeTime: Joi.number().positive().min(0).max(25).precision(2),
+  dailyNorm: Joi.number().positive().min(0).max(15000).precision(0),
 });
