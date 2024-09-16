1b5f9f770f42e41bcb8b411d6fe49eebee672fea
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-09-16 22:30:58 +0300

    fix-check-email

--- src/controllers/usersUpdateController.js
+++ src/controllers/usersUpdateController.js
@@ -1,17 +1,28 @@
 import createHttpError from 'http-errors';
-import { updateUsersById } from '../services/authUserService.js';
+import {
+  getAuthUserByEmail,
+  updateUsersById,
+} from '../services/authUserService.js';
 
 export const usersUpdateController = async (req, res, next) => {
   const userId = req.authUser._id;
   const { email } = req.body;
 
-  const checkEmail = await getAuthUserByEmail(email);
+  /* если front-end передвает нам email
+  мы ищем его в db, если его нет, значит нам передали новый email делаем замену,
+  если он есть в db проеряем принаджежит нашему пользоваетлю: то его дали просто со всеми данными
+  если если он принаджужит другому пользователю получится ошибка */
 
-  if (checkEmail)
-    throw createHttpError(
-      409,
-      'This email is already in use and cannot be changed',
-    );
+  if (email) {
+    const userEmail = await getAuthUserByEmail(email);
+
+    if (userEmail !== null && userId !== userEmail._id) {
+      throw createHttpError(
+        409,
+        'This email is already in use and cannot be changed',
+      );
+    }
+  }
 
   const user = await updateUsersById(userId, req.body);
 
